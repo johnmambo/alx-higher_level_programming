cebb97b1f76e23c6f83ff6a2f3a034a93eb54171
johnmambo <82521915+johnmambo@users.noreply.github.com>
2022-06-02 12:27:36 -0700

    hide those

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
+
+def principal():
     for i in dir(hidden_4):
-        if i[2] != "__":
+        if not (i[0] == '_' and i[1] == '_'):
             print(i)
+
+if __name__ == "__main__":
+    principal()
