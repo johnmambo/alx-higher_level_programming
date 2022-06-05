a6c53057ff433f2af56961f17a470f7e20fb82a2
johnmambo <johnmambo794@gmail.com>
2022-06-05 12:04:39 -0700

    list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    return (my_list[idx] if 0 <= idx < len(my_list) else None)
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
